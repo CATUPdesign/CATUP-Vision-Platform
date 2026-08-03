3ec8a904f91fde1fa82c9e19d1742ebf71aa4bd1
CATUPdesign <khaekgb@gmail.com>
2026-08-03 08:26:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import cv2
+import time
 from camera.camera import Camera
 
 
 def main():
 
     cam = Camera()
+    prev_time = time.time()
 
     while True:
 
         frame = cam.read()
+        height, width = frame.shape[:2]
+
+        cv2.line(frame, (width//2 - 20, height//2),
+                (width//2 + 20, height//2), (0,255,0), 2)
+
+        cv2.line(frame, (width//2, height//2 - 20),
+                (width//2, height//2 + 20), (0,255,0), 2)
+
+        current_time = time.time()
+        fps = 1 / (current_time - prev_time)
+        prev_time = current_time
+
+        cv2.putText(frame,
+                    f"FPS: {fps:.1f}",
+                    (10,30),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.8,
+                    (0,255,0),
+                    2)
 
         cv2.imshow("CATUP Vision", frame)
 
